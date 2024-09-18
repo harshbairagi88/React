91576f1f8912ac7ab93ad2740d3ce11a01bc79ee
hemnt dhote <hemantdhote76@gmail.com>
2024-09-18 18:16:47 +0530

    bhai commit bhi kar diya

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const App = () => {
   return (
     <div>
-      <h1>tailwind is integrated </h1>
+      <h1>tailwind is integrated  add this file</h1>
     </div>
   )
 }
